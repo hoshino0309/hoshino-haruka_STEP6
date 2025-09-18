d335659cadb5eb46ca2d8ba3eb09af4e99c0fc72
user <user@usernoMacBook-Air.local>
2025-09-18 16:52:29 +0900

    add project files

---
+++ style.js
@@ -0,0 +1,55 @@
+//送信する
+let sendButton = document.getElementById('sendButton');
+
+sendButton.addEventListener('click', (e) => {
+    // ボタンがクリックされた時点で値を取得
+    let username = document.getElementById('username').value.trim();
+    let companyName = document.getElementById('companyName').value.trim();
+    let email = document.getElementById('email').value.trim();
+    let message = document.getElementById('message').value.trim();
+
+    if (username === "") {
+        alert('必須項目が未入力です。入力内容をご確認ください。');
+        e.preventDefault(); // 送信をキャンセル
+        return false;
+    }
+    if (companyName === "") {
+        alert('必須項目が未入力です。入力内容をご確認ください。');
+        e.preventDefault(); // 送信をキャンセル
+        return false;
+    }
+    if (email === "") {
+        alert('必須項目が未入力です。入力内容をご確認ください。');
+        e.preventDefault(); // 送信をキャンセル
+        return false;
+    }
+    if (message === "") {
+        alert('必須項目が未入力です。入力内容をご確認ください。');
+        e.preventDefault(); // 送信をキャンセル
+        return false;
+    }
+
+    let content = `以下の内容を本当に送信しますか？\n\n` +
+        `お名前：${username}\n` +
+        `会社名：${companyName}\n` +
+        `メールアドレス：${email}\n` +
+        `お問い合わせ内容：${message}`;
+    
+    let isConfirmed = confirm(content);
+    if (!isConfirmed) {
+        e.preventDefault();
+        return false;
+    }
+});
+
+//footer
+const colors = ['blue', 'red', 'yellow', 'gray'];
+let colorIndex = 0;
+
+const footer = document.querySelector('footer');
+const changeButton = document.querySelector('footer input[type="button"]')
+
+changeButton.addEventListener('click', () => {
+    footer.style.backgroundColor = colors[colorIndex];
+    colorIndex = (colorIndex + 1) % colors.length;
+});
